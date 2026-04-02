4dff0073d44ea8ddb0ade3a533b0afbbb68c9824
ThaiMinh3101 <minhroll2k@gmail.com>
2026-04-02 19:32:27 +0700

    Update bt2-mang1chieu.cpp

--- làm bài tập/bt2-mang1chieu.cpp
+++ làm bài tập/bt2-mang1chieu.cpp
@@ -7,21 +7,28 @@ void tongmang(int a[], int N) {
     }
     cout << "Tong cac so trong mang la: " << tong << endl;
 }
-void phantuduongnhonhat (int a[], int N) {
+void phantuduongnhonhat(int a[], int N) {
     int min = 0;
     for (int i = 0; i < N; i++) {
-        if (a[i] > 0) {
-            min = a[i];
-            break;
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        if (a[i] > 0 && a[i] < min) {
-            min = a[i];
+        if (a[i] > 0 && a[i] % 2 == 0) {
+            if (min == 0 || a[i] < min)
+                min = a[i];
         }
     }
     if (min > 0)
         cout << "Phan tu duong nho nhat trong mang la: " << min << endl;
     else
         cout << "Khong co phan tu duong trong mang." << endl;
+}
+int main() {
+    int a[100];
+    int N;
+    cout << "nhap N: ";
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cout << "nhap mang a[" << i << "]: ";
+        cin >> a[i];
+    }
+    tongmang(a, N);
+    phantuduongnhonhat(a, N);
 }
