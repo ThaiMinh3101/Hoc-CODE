fd0796b64354a259d30b618bbb405529969a1715
ThaiMinh3101 <minhroll2k@gmail.com>
2026-08-28 10:10:41 +0700

    cau 3

--- năm 2/TH3-2.cpp
+++ năm 2/TH3-2.cpp
@@ -1,61 +1,84 @@
-//Bài 2: Cài đặt thuật toán tìm kiếm nhị phân:
+// Bài 2: Cài đặt thuật toán tìm kiếm nhị phân:
 #include <iostream>
 using namespace std;
-void HoanVi(int &a, int &b) {
+void HoanVi(int &a, int &b)
+{
     // Hoán vị hai số nguyên
     int temp = a;
     a = b;
     b = temp;
 }
-void SapXepTangDan(int a[], int N) {
+void SapXepTangDan(int a[], int N)
+{
     for (int i = 0; i < N; i++)
         for (int j = i + 1; j < N; j++)
             if (a[i] > a[j])
                 HoanVi(a[i], a[j]);
 }
-void XuatLeftRight(int a[],int left, int right) {
-    cout<< "Tu Left den Right: ";
-    for (int i = left; i <= right; i++) {
-        cout<<a[i]<<" ";
+void XuatLeftRight(int a[], int left, int right)
+{
+    cout << "Gia tri tu Left den Right: ";
+    for (int i = left; i <= right; i++)
+    {
+        cout << a[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-int BinarySearch(int a[], int N, int X) {
+int BinarySearch(int a[], int N, int X)
+{
     SapXepTangDan(a, N);
     int left = 0;
     int right = N - 1;
-    while (left <= right) {
+    while (left <= right)
+    {
         int mid = (left + right) / 2;
-        if (a[mid] == X) {
+        cout << "Mang da phan chia: ";
+        for (int i = mid; i <= right; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        cout << "Thuc hien so sanh: " << a[mid] << " va " << X << endl;
+        if (a[mid] == X)
+        {
             return mid;
         }
-        else if (a[mid] < X) {
+        else if (a[mid] < X)
+        {
             left = mid + 1;
+            cout << "Vi tri cua Left: " << left << ", Right: " << right << endl;
             XuatLeftRight(a, left, right);
         }
-        else {
+        else
+        {
             right = mid - 1;
+            cout << "Vi tri cua Left: " << left << ", Right: " << right << endl;
             XuatLeftRight(a, left, right);
         }
     }
     return -1;
 }
-int main() {
+int main()
+{
     int a[100];
     int N;
-    cout<<"Nhap so phan tu: ";
-    cin>>N;
-    for (int i = 0; i < N; i++) {
+    cout << "Nhap so phan tu: ";
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
         cout << "Nhap phan tu a[" << i << "]: ";
         cin >> a[i];
     }
     int X;
-    cout<<"Nhap gia tri can tim: ";
-    cin>>X;
+    cout << "Nhap gia tri can tim: ";
+    cin >> X;
     int kq = BinarySearch(a, N, X);
-    if (kq != -1) {
+    if (kq != -1)
+    {
         cout << "Tim thay " << X << " tai vi tri " << kq << endl;
-    } else {
+    }
+    else
+    {
         cout << "Khong tim thay " << X << " trong mang" << endl;
     }
     return 0;
