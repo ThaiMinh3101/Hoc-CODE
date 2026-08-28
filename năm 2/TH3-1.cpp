d0f462c44e8ef45bb3385bf07ec08da58a4d176d
ThaiMinh3101 <minhroll2k@gmail.com>
2026-08-28 10:39:16 +0700

    .

--- năm 2/TH3-1.cpp
+++ năm 2/TH3-1.cpp
@@ -26,18 +26,18 @@ int main()
 {
 int a[50];
 int N;
-cout << "Nhap so phan tu cua mang:";
+cout << "Nhap so phan tu cua mang: ";
 cin >> N;
 NhapMang(a, N);
 XuatMang(a, N);
 int X;
-cout << "\nNhap gia tri X can tim:";
+cout << "Nhap gia tri X can tim: ";
 cin >> X;
 int kq;
 kq = LinearSearch(a, N, X);
 if (kq == -1)
 cout<<"\nKhong co " <<X<<" trong mang"<<endl;
 else
-cout<<"\n"<< X<<" xuat hien o vi tri "<<kq<<"trong mang"<<endl;
+cout<<"\n"<< X<<" xuat hien o vi tri "<<kq<<" trong mang"<<endl;
 return 0;
 }
