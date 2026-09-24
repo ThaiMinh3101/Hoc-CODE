6af8782761be296b6bb7ef7eade9cbe6d73c1f09
ThaiMinh3101 <minhroll2k@gmail.com>
2026-09-24 23:03:48 +0700

    .

---
+++ năm 2/làm bài tập/NhapMangNgauNhien.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void NhapMang(int a[], int N)
+{
+    srand(time(0));
+    for (int i = 0; i < N; i++)
+        a[i] = rand() % 50;
+}
--- năm 2/làm bài tập/TH4.cpp
+++ năm 2/làm bài tập/TH4.cpp
@@ -3,9 +3,10 @@
 #include<ctime>
 using namespace std;
 void NhapMang(int a[], int N) {
-    srand(time(0));
-    for (int i = 0; i < N; i++)
-        a[i] = rand() % 50;
+    for (int i = 0; i < N; i++) {
+        cout<<"Nhap a["<<i<<"] = ";
+        cin>>a[i];
+    }
 }
 void XuatMang(int a[], int N) {
     for (int i = 0; i < N; i++) {
